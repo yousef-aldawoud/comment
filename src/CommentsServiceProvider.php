43e39857834ed20c1a12f1e7eed51def74bd53e2
Yousef Aldawoud <yousef.l.t.aldawoud@protonmail.com>
2020-04-08 10:33:38 +1200

    publishing migration files

--- src/CommentsServiceProvider.php
+++ src/CommentsServiceProvider.php
@@ -7,6 +7,9 @@ class CommentsServiceProvider extends ServiceProvider {
 
     public function boot(){
         $this->loadMigrationsFrom(__DIR__.'/migrations');
+        $this->publishes([
+            __DIR__.'/migrations' => database_path('migrations')
+        ], 'migrations');
     }
 
     public function register(){
